a88026f0c4161588995bc2b3aefa1d125b521114
ehanrspp <92194938+ehanrspp@users.noreply.github.com>
2022-03-13 19:50:09 +0700

    Add files via upload

---
+++ TugasMinggu5/Kasus 3/Sasuke.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kasus.pkg3;
+
+/**
+ *
+ * @author Raii
+ */
+public class Sasuke extends Itachi{
+    String Dojutsu = "Sharingan";
+    
+void printDojutsu() {
+    super.printDojutsu();
+    System.out.println(this.Dojutsu);
+}
+}
