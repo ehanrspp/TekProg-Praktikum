5f87f7c0ab5628a22528bd7c98bb2031ed1082c7
ehanrspp <92194938+ehanrspp@users.noreply.github.com>
2022-04-10 13:49:28 +0700

    Add files via upload

---
+++ TugasMinggu9/Firm.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package task7.pkg3;
+
+/**
+ *
+ * @author Raii
+ */
+public class Firm {
+
+    //----------------------------------------------------------
+    // Creates a staff of employees for a firm and pays them.
+    //----------------------------------------------------------
+    public static void main (String[] args){
+    Staff personnel = new Staff();
+    personnel.payday();
+    }
+}
